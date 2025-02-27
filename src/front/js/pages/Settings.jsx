a3b8a52d24205de9ddc20a3a11ec5f2a3ddd4409
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2025-02-27 20:27:17 +0000

    navbar y footer done

--- src/front/js/pages/Settings.jsx
+++ src/front/js/pages/Settings.jsx
@@ -3,8 +3,26 @@ import React from "react";
 export const Settings = () => {
 
     return (
-        <div>
-            <h1>Mi perfil</h1>
+        <div className="text-center mt-2">
+            <h1>Hola, Marcos</h1>
+            <form>
+                <div class="form-group">
+                    <label for="formGroupExampleInput">Username</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput1" placeholder="Example input" />
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Email</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input" />
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Password</label>
+                    <input type="password" class="form-control" id="formGroupExampleInput3" placeholder="Another input" />
+                </div>
+                <div class="form-group">
+                    <label for="formGroupExampleInput2">Phone number</label>
+                    <input type="text" class="form-control" id="formGroupExampleInput4" placeholder="Another input" />
+                </div>
+            </form>
         </div>
     )
 }
